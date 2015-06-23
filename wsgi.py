79328dfafccd4e4d1cd3453785edadf6d99dde60
40223115 <40223115@gm.nfu.edu.tw>
2015-06-23 09:26:19 +0800

    40223115

--- wsgi.py
+++ wsgi.py
@@ -1095,6 +1095,263 @@ class Hello(object):
 
         return outstring
     @cherrypy.expose
+    # N 為齒數, M 為模數, P 為壓力角
+    def w17(self, N=20, N1=10, N2=30, N3=10, N4=20, N5=30, N6=30,M=5, P=15):
+        outstring = '''
+    <!DOCTYPE html> 
+    <html>
+    <head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    </head>
+    <body>
+        
+    <form method=POST action=w17_test>
+    第1齒數:<br />
+        <select name="N">
+        '''
+        for j in range(15,86):
+            outstring+=''' <option value="'''+str(j)+'''">'''+str(j)+'''</option>'''
+        outstring+='''
+       </select><br/>
+    第2齒數:<br />
+        <select name="N1">
+        '''
+        for j in range(15,86):
+            outstring+=''' <option value="'''+str(j)+'''">'''+str(j)+'''</option>'''
+        outstring+='''
+       </select><br/>
+    第3齒數:<br />
+        <select name="N2">
+        '''
+        for j in range(15,86):
+            outstring+=''' <option value="'''+str(j)+'''">'''+str(j)+'''</option>'''
+        outstring+='''
+       </select><br/>
+    第4齒數:<br />
+        <select name="N3">
+        '''
+        for j in range(15,86):
+            outstring+=''' <option value="'''+str(j)+'''">'''+str(j)+'''</option>'''
+        outstring+='''
+       </select><br/>
+    第5齒數:<br />
+        <select name="N4">
+        '''
+        for j in range(15,86):
+            outstring+=''' <option value="'''+str(j)+'''">'''+str(j)+'''</option>'''
+        outstring+='''
+       </select><br/>
+    第6齒數:<br />
+        <select name="N5">
+        '''
+        for j in range(15,86):
+            outstring+=''' <option value="'''+str(j)+'''">'''+str(j)+'''</option>'''
+        outstring+='''
+       </select><br/>
+
+    模數  :<input type=text name=M value='''+str(M)+'''><br />
+    壓力角:<input type=text name=P value = '''+str(P)+'''><br />
+    <input type=submit value=畫出正齒輪輪廓>
+    </form>
+    <br /><a href="index2">index2</a><br />
+    <!-- 載入 brython.js -->
+    <script type="text/javascript" src="/static/Brython3.1.1-20150328-091302/brython.js"></script>
+    <script>
+    window.onload=function(){
+    brython();
+    }
+    </script>
+    </body>
+    </html>
+    '''
+
+        return outstring
+    @cherrypy.expose
+    # N 為齒數, M 為模數, P 為壓力角
+    def w17_test(self, N=20, N1=10, N2=30, N3=10, N4=20, N5=30, N6=30,M=15, P=15):
+        outstring = '''
+    <!DOCTYPE html> 
+    <html>
+    <head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <!-- 載入 brython.js -->
+    <script type="text/javascript" src="/static/Brython3.1.1-20150328-091302/brython.js"></script>
+    <script src="/static/Cango2D.js" type="text/javascript"></script>
+    <script src="/static/gearUtils-04.js" type="text/javascript"></script>
+    </head>
+    <!-- 啟動 brython() -->
+    <body onload="brython()">
+
+    第1齒數:'''+str(N)+'''<output name=N for=str(N)><br />
+    第2齒數:'''+str(N1)+'''<output name=N1 for=str(N1)><br />
+    第3齒數:'''+str(N2)+'''<output name=N2 for=str(N2)><br />
+    第4齒數:'''+str(N3)+'''<output name=N3 for=str(N3)><br />
+    第5齒數:'''+str(N4)+'''<output name=N4 for=str(N4)><br />
+    第6齒數:'''+str(N5)+'''<output name=N5 for=str(N5)><br />
+
+    模數:'''+str(M)+'''<output name=M for=str(M)><br />
+    壓力角:'''+str(P)+'''<output name=P for=str(P)><br />
+    齒數比:'''+str(N)+''':'''+str(N1)+''':'''+str(N2)+''':'''+str(N3)+''':'''+str(N4)+''':'''+str(N5)+''':'''+str(N6)+'''<br />
+
+    <!-- 以下為 canvas 畫圖程式 -->
+    <script type="text/python">
+    # 從 browser 導入 document
+    from browser import document
+    from math import *
+    # 請注意, 這裡導入位於 Lib/site-packages 目錄下的 spur.py 檔案
+    import spur
+
+    # 準備在 id="plotarea" 的 canvas 中繪圖
+    canvas = document["plotarea"]
+    ctx = canvas.getContext("2d")
+
+    # 以下利用 spur.py 程式進行繪圖, 接下來的協同設計運算必須要配合使用者的需求進行設計運算與繪圖
+    # 其中並將工作分配給其他組員建立類似 spur.py 的相關零件繪圖模組
+    # midx, midy 為齒輪圓心座標, rp 為節圓半徑, n 為齒數, pa 為壓力角, color 為線的顏色
+    # Gear(midx, midy, rp, n=20, pa=20, color="black"):
+    # 模數決定齒的尺寸大小, 囓合齒輪組必須有相同的模數與壓力角
+    # 壓力角 pa 單位為角度
+    pa = 20
+    # m 為模數
+    m = '''+str(M)+'''
+    # 第1齒輪齒數
+    n_g1 = '''+str(N)+'''
+    # 第2齒輪齒數
+    n_g2 = '''+str(N1)+'''
+    # 第3齒輪齒數
+    n_g3 ='''+str(N2)+'''
+    # 第4齒輪齒數
+    n_g4 ='''+str(N3)+'''
+    # 第5齒輪齒數
+    n_g5 ='''+str(N4)+'''
+    # 第6齒輪齒數
+    n_g6 ='''+str(N5)+'''
+
+
+    # 計算兩齒輪的節圓半徑
+    rp_g1 = m*n_g1/2
+    rp_g2 = m*n_g2/2
+    rp_g3 = m*n_g3/2
+    rp_g4 = m*n_g4/2
+    rp_g5= m*n_g5/2
+    rp_g6= m*n_g6/2
+
+    # 繪圖第1齒輪的圓心座標
+    x_g1 = 400
+    y_g1 = 400
+    # 第2齒輪的圓心座標, 假設排列成水平, 表示各齒輪圓心 y 座標相同
+    x_g2 = x_g1
+    y_g2 = y_g1+rp_g1 + rp_g2
+
+    # 第3齒輪的圓心座標
+    x_g3 = x_g2+ rp_g2+rp_g3
+    y_g3 = y_g2
+
+    # 第4齒輪的圓心座標
+    x_g4 = x_g3
+    y_g4 = y_g3 + rp_g3+rp_g4
+
+    # 第5齒輪的圓心座標
+    x_g5 = x_g4+ rp_g4+rp_g5
+    y_g5 = y_g4
+
+    # 第6齒輪的圓心座標
+    x_g6 = x_g5
+    y_g6 = y_g5 + rp_g5+rp_g6
+
+    # 將第1齒輪順時鐘轉 90 度
+    # 使用 ctx.save() 與 ctx.restore() 以確保各齒輪以相對座標進行旋轉繪圖
+
+    ctx.font = "10px Verdana";
+    ctx.fillText("組員:",x_g1-20, y_g1-10);
+
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g1, y_g1)
+    # rotate to engage
+    ctx.rotate(pi)
+    # put it back
+    ctx.translate(-x_g1, -y_g1)
+    spur.Spur(ctx).Gear(x_g1, y_g1, rp_g1, n_g1, pa, "blue")
+    ctx.restore()
+
+    # 將第2齒輪逆時鐘轉 90 度之後, 再多轉一齒, 以便與第1齒輪進行囓合
+
+    ctx.font = "10px Verdana";
+    ctx.fillText("組員:",x_g2-20, y_g2-10);
+
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g2, y_g2)
+    # rotate to engage
+    ctx.rotate(pi/n_g2)
+    # put it back
+    ctx.translate(-x_g2, -y_g2)
+    spur.Spur(ctx).Gear(x_g2, y_g2, rp_g2, n_g2, pa, "black")
+    ctx.restore()
+
+    # 將第3齒輪順時鐘轉 90 度
+    # 使用 ctx.save() 與 ctx.restore() 以確保各齒輪以相對座標進行旋轉繪圖
+
+    ctx.font = "10px Verdana";
+    ctx.fillText("組員:",x_g3-20, y_g3-10);
+
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g3, y_g3)
+    # rotate to engage
+    ctx.rotate(-pi/2-pi/n_g3+(pi/2+pi/n_g2)*n_g2/n_g3)
+    # put it back
+    ctx.translate(-x_g3, -y_g3)
+    spur.Spur(ctx).Gear(x_g3, y_g3, rp_g3, n_g3, pa, "blue")
+    ctx.restore()
+
+    # 將第4齒輪逆時鐘轉 90 度之後, 再多轉一齒, 以便與第1齒輪進行囓合
+
+    ctx.font = "10px Verdana";
+    ctx.fillText("組員:",x_g4-20, y_g4-10);
+
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g4, y_g4)
+    # rotate to engage
+    ctx.rotate(-pi/n_g4+(-pi/2+pi/n_g3)*n_g3/n_g4-(pi/2+pi/n_g2)*n_g2/n_g4)
+    # put it back
+    ctx.translate(-x_g4, -y_g4)
+    spur.Spur(ctx).Gear(x_g4, y_g4, rp_g4, n_g4, pa, "black")
+    ctx.restore()
+
+    #第5齒輪
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g5, y_g5)
+    # rotate to engage
+    ctx.rotate(-pi/2-pi/n_g5+((pi/2+pi/n_g4)+(-pi/2+pi/n_g3)*n_g3/n_g4-(pi/2+pi/n_g2)*n_g2/n_g4)*n_g4/n_g5)
+    # put it back
+    ctx.translate(-x_g5, -y_g5)
+    spur.Spur(ctx).Gear(x_g5, y_g5, rp_g5, n_g5, pa, "purple")
+    ctx.restore()
+
+    #第6齒輪
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g6, y_g6)
+    # rotate to engage
+    ctx.rotate(-pi/n_g6+(-pi/2+pi/n_g5)*n_g5/n_g6-(pi/2+pi/n_g4)*n_g4/n_g6-(pi/2+pi/n_g3)*n_g3/n_g6-(pi/2+pi/n_g2)*n_g2/n_g6)
+    # put it back
+    ctx.translate(-x_g6, -y_g6)
+    spur.Spur(ctx).Gear(x_g6, y_g6, rp_g6, n_g6, pa, "blue")
+    ctx.restore()
+
+
+    </script>
+    <canvas id="plotarea" width="3000" height="3000"></canvas>
+    </body>
+    </html>
+    '''
+
+        return outstring
+    @cherrypy.expose
     def doCheck(self, guess=None):
         # 假如使用者直接執行 doCheck, 則設法轉回根方法
         if guess is None:
